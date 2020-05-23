1254df0ef7d0d2b28a818ce2a15b9d9485c90f3b
Gustavo780904 <gustavodutrasantos@gmail.com>
2020-05-22 21:59:15 -0300

    Correção: Validadcao dos dados Leitura do nome e salario

--- src/simuladoFinanciamento/Simulado.java
+++ src/simuladoFinanciamento/Simulado.java
@@ -8,14 +8,16 @@ public class Simulado {
 	public static void main(String[] args) {
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
-		
-		
+	
 		
 		System.out.println("DIGITE OS DADOS DO FINANCIAMENTO");
 		System.out.print("Nome do cliente: ");
 		String nome = sc.nextLine();
+		double salario;
+		do {
 		System.out.print("Salario: ");
-		double salario = sc.nextDouble();
+		salario = sc.nextDouble();
+		} while (salario < 0);	
 		
 		
 		sc.close();
